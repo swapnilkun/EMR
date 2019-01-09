fc5b0e45c72a28b0f3b62fe794d8a20744126ba7
Swapnil Kunkekar <swapnil93kunkekar@gmail.com>
2019-01-09 23:57:15 +0530

    Image Optimization, CSS, Some Responsive changes
    In Progress

--- ExcellentMarketResearch/App_Start/RouteConfig.cs
+++ ExcellentMarketResearch/App_Start/RouteConfig.cs
@@ -12,6 +12,7 @@ namespace ExcellentMarketResearch
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            routes.RouteExistingFiles = true;
             routes.MapRoute(
              name: "Sitemap1",
              url: "sitemap{PageNo}.xml",
